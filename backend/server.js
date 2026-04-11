f31cf9ae16e535fea9bf9d68b8f8562a001e211f
Mohit <mohit@Mohits-MacBook-Pro.local>
2026-04-12 00:02:45 +0530

    Handling db connection

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const userLogin = require("./user-login/login");
+const { connectDB } = require("./utility/database");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Connect to MongoDB
+connectDB();
+
 app.use(userLogin);
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
